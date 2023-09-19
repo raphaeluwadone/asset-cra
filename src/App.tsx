bcc6381b339088df066ee7c25faf546cce82d718
raphael_u@bts.com.ng <raphael_u@bts.com.ng>
2023-09-19 07:14:06 +0100

    removed base url

--- src/App.tsx
+++ src/App.tsx
@@ -44,80 +44,80 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path={`${BASE_URL}/`} element={<LandingPage />} />
-        <Route path={`${BASE_URL}/dashboard`} element={<Dashboard />} />
-        <Route path={`${BASE_URL}/requisition`} element={<Requisition />} />
-        <Route path={`${BASE_URL}/pricelist`} element={<PriceList />} />
-        <Route path={`${BASE_URL}/register/:planId`} element={<Register />} />
-        <Route path={`${BASE_URL}/confirmemail`} element={<ConfirmEmail />} />
-        <Route path={`${BASE_URL}/reset-password`} element={<ForgetPassword />} />
+        <Route path={`/`} element={<LandingPage />} />
+        <Route path={`/dashboard`} element={<Dashboard />} />
+        <Route path={`/requisition`} element={<Requisition />} />
+        <Route path={`/pricelist`} element={<PriceList />} />
+        <Route path={`/register/:planId`} element={<Register />} />
+        <Route path={`/confirmemail`} element={<ConfirmEmail />} />
+        <Route path={`/reset-password`} element={<ForgetPassword />} />
         <Route
-          path={`${BASE_URL}/requisition/management`}
+          path={`/requisition/management`}
           element={<RequisitionManagement />}
         />
-        <Route path={`${BASE_URL}/login`} element={<Login />} />
+        <Route path={`/login`} element={<Login />} />
         <Route
-          path={`${BASE_URL}/reset-password`}
+          path={`/reset-password`}
           element={<ForgetPassword />}
         />
         <Route
-          path={`${BASE_URL}/requisition/unavailable`}
+          path={`/requisition/unavailable`}
           element={<UnavailableRequisition />}
         />
         <Route
-          path={`${BASE_URL}/requisition/approvals`}
+          path={`/requisition/approvals`}
           element={<RequisitionApprovals />}
         />
         <Route
-          path={`${BASE_URL}/requisition/returns`}
+          path={`/requisition/returns`}
           element={<RequisitionReturns />}
         />
-        <Route path={`${BASE_URL}/store/`} element={<Store />} />
+        <Route path={`/store/`} element={<Store />} />
         <Route
-          path={`${BASE_URL}/store/categories`}
+          path={`/store/categories`}
           element={<StoreCategories />}
         />
-        <Route path={`${BASE_URL}/store/items`} element={<StoreItems />} />
-        <Route path={`${BASE_URL}/store/stocks`} element={<StoreStocks />} />
-        <Route path={`${BASE_URL}/fulfillment`} element={<Fulfillment />} />
+        <Route path={`/store/items`} element={<StoreItems />} />
+        <Route path={`/store/stocks`} element={<StoreStocks />} />
+        <Route path={`/fulfillment`} element={<Fulfillment />} />
         <Route
-          path={`${BASE_URL}/fulfillment/approved`}
+          path={`/fulfillment/approved`}
           element={<ApprovedFulfilment />}
         />
         <Route
-          path={`${BASE_URL}/fulfillment/unavailable`}
+          path={`/fulfillment/unavailable`}
           element={<UnavailableFulfilment />}
         />
-        <Route path={`${BASE_URL}/organization`} element={<Organization />} />
+        <Route path={`/organization`} element={<Organization />} />
         <Route
-          path={`${BASE_URL}/organization/information`}
+          path={`/organization/information`}
           element={<OrganizationInformation />}
         />
         <Route
-          path={`${BASE_URL}/organization/branch`}
+          path={`/organization/branch`}
           element={<OrganizationBranch />}
         />
         <Route
-          path={`${BASE_URL}/organization/department`}
+          path={`/organization/department`}
           element={<OrganizationDepartment />}
         />
         <Route
-          path={`${BASE_URL}/organization/position`}
+          path={`/organization/position`}
           element={<OrganizationPosition />}
         />
-        <Route path={`${BASE_URL}/settings`} element={<Settings />} />
-        <Route path={`${BASE_URL}/settings/menu`} element={<Menu />} />
-        <Route path={`${BASE_URL}/settings/workflow`} element={<WorkFlow />} />
-        <Route path={`${BASE_URL}/users`} element={<UserManagement />} />
-        <Route path={`${BASE_URL}/users/records`} element={<UserRecords />} />
-        <Route path={`${BASE_URL}/users/roles`} element={<UserRoles />} />
-        <Route path={`${BASE_URL}/users/movement`} element={<Movement />} />
-        <Route path={`${BASE_URL}/printing`} element={<Printing />} />
-        <Route path={`${BASE_URL}/printing/search`} element={<Search />} />
-        <Route path={`${BASE_URL}/printing/tags`} element={<Tags />} />
-        <Route path={`${BASE_URL}/printing/printing`} element={<Print />} />
-        <Route path={`${BASE_URL}/printing/download`} element={<Download />} />
-        <Route path={`${BASE_URL}/printing/export`} element={<Export />} />
+        <Route path={`/settings`} element={<Settings />} />
+        <Route path={`/settings/menu`} element={<Menu />} />
+        <Route path={`/settings/workflow`} element={<WorkFlow />} />
+        <Route path={`/users`} element={<UserManagement />} />
+        <Route path={`/users/records`} element={<UserRecords />} />
+        <Route path={`/users/roles`} element={<UserRoles />} />
+        <Route path={`/users/movement`} element={<Movement />} />
+        <Route path={`/printing`} element={<Printing />} />
+        <Route path={`/printing/search`} element={<Search />} />
+        <Route path={`/printing/tags`} element={<Tags />} />
+        <Route path={`/printing/printing`} element={<Print />} />
+        <Route path={`/printing/download`} element={<Download />} />
+        <Route path={`/printing/export`} element={<Export />} />
       </Routes>
     </>
   );
